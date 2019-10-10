794501fcb7a46f73e185bc38749fee81b0474133
e-coder-code <buildtechempire@gmail.com>
2019-10-10 18:44:48 +0000

    4.6 is done

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -15,3 +15,16 @@ Outbound Interface:    FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+pro, pre, ad, _, n_hop, l_up, intf = ospf_route.split()
+
+print(
+f"""
+Protocol:              {pro}SPF
+Prefix:                {pre}
+AD/Metric:             {ad.strip('[]')}
+Next-Hop:              {n_hop.rstrip(',')}
+Last update:           {l_up.rstrip(',')}
+Outbound Interface:    {intf}
+"""
+)
+
